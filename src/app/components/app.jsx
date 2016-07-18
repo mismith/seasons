b9aabc37a2a5860a8c67fea76a0bd1dad8ad3cee
Murray Smith <murray@mismith.info>
2016-07-18 09:33:12 -0600

    add drawer

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
 
 // material-ui
-import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+import AddIcon from 'material-ui/svg-icons/content/add';
+import PowerSettingsIcon from 'material-ui/svg-icons/action/power-settings-new';
+
 const App = React.createClass({
+	getInitialState() {
+		return {
+			drawerOpen: false,
+		};
+	},
+	handleDrawerToggle() {
+		this.setState({drawerOpen: !this.state.drawerOpen});
+	},
+	handleDrawerClose() {
+		this.setState({drawerOpen: false});
+	},
 	render() {
 		return (
 			<MuiThemeProvider>
 				<div>
-					<AppBar title="Seasons" />
+					<AppBar
+						title="Seasons"
+						onLeftIconButtonTouchTap={this.handleDrawerToggle}
+					/>
+					<Drawer
+						docked={false}
+						open={this.state.drawerOpen}
+					>
+						<List onTouchTap={this.handleDrawerClose}>
+							<Subheader>Seasons</Subheader>
+							<ListItem>Flames 2016/2017</ListItem>
+							<ListItem rightIcon={<AddIcon />}>Add New Season</ListItem>
+							<Divider />
+							<Subheader>Account</Subheader>
+							<ListItem rightIcon={<PowerSettingsIcon />}>Logout</ListItem>
+						</List>
+					</Drawer>
 					<main>
 						{this.props.children}
 					</main>
