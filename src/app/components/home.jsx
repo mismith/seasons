bca75348df4467fa48442d260b26a0498cf4ed7b
Murray Smith <murray@mismith.info>
2016-07-18 09:33:00 -0600

    add tabs

--- src/app/components/home.jsx
+++ src/app/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 
+import {Tabs, Tab} from 'material-ui/Tabs';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
@@ -30,29 +31,34 @@ const Home = React.createClass({
 		});
 
 		return (
-			<div>
-				<List>
-				{gamesByMonth.map((games, monthIndex) =>
-				<div key={monthIndex}>
-					<Subheader>{games[0].$datetime.format('MMMM')} {games[0].$datetime.format('M') === '1' && games[0].$datetime.format('YYYY')}</Subheader>
-					{games.map(game => 
-					<ListItem
-						key={game.$id}
-						primaryText={game.opponent}
-						secondaryText={game.$datetime.format('h:mma, ddd, MMM D, YYYY')}
-						leftAvatar={<Avatar>{game.$datetime.format('D')}</Avatar>}
-						rightAvatar={<div style={{display: 'inline-flex', marginTop: 6}}>
-						{Data.seasons[this.props.seasonId].seats.map((seat, seatId) => 
-							<SeatAvatar key={seatId} info={seat} data={Data['seasons:data'][this.props.seasonId].seats[game.$id][seatId]} game={game} />
+			<Tabs>
+				<Tab label="Schedule">
+					<List>
+					{gamesByMonth.map((games, monthIndex) =>
+					<div key={monthIndex}>
+						<Subheader>{games[0].$datetime.format('MMMM')} {games[0].$datetime.format('M') === '1' && games[0].$datetime.format('YYYY')}</Subheader>
+						{games.map(game => 
+						<ListItem
+							key={game.$id}
+							primaryText={game.opponent}
+							secondaryText={game.$datetime.format('h:mma, ddd, MMM D, YYYY')}
+							leftAvatar={<Avatar>{game.$datetime.format('D')}</Avatar>}
+							rightAvatar={<div style={{display: 'inline-flex', marginTop: 6}}>
+							{Data.seasons[this.props.seasonId].seats.map((seat, seatId) => 
+								<SeatAvatar key={seatId} info={seat} data={(Data['seasons:data'][this.props.seasonId].seats[game.$id] || [])[seatId]} game={game} />
+							)}
+							</div>}
+							innerDivStyle={{paddingRight: 56 + 12 + 32 * (Data.seasons[this.props.seasonId].seats.length - 1)}}
+						/>
 						)}
-						</div>}
-						innerDivStyle={{paddingRight: 56 + 12 + 32 * (Data.seasons[this.props.seasonId].seats.length - 1)}}
-					/>
+					</div>
 					)}
-				</div>
-				)}
-				</List>
-			</div>
+					</List>
+				</Tab>
+				<Tab label="Stats">
+					<Subheader>Stats will go here.</Subheader>
+				</Tab>
+			</Tabs>
 		)
 	},
 });
