bf6e3685a5c520ec8cc90c7cc7036499d300e436
Murray Smith <murray@businessinstincts.com>
2016-09-20 13:42:42 -0600

    switch back to [hash].[ext] filenames since it doesn’t cache bust either way

--- webpack/webpack-prod.config.js
+++ webpack/webpack-prod.config.js
@@ -1,6 +1,6 @@
 module.exports = require('./webpack.config.js')({
   isProduction: true,
   devtool: 'source-map',
-  jsFileName: 'app.js?v=[hash]',
-  cssFileName: 'app.css?v=[hash]',
+  jsFileName: 'app.[hash].js',
+  cssFileName: 'app.[hash].css',
 });
